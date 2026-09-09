8aab4074f7ca9ffa810c378fd547fbfbe8507f92
bes <bes237311@gmail.com>
2026-09-09 21:45:00 +0500

    Task3

---
+++ Tyuiu.BalakinES.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Tyuiu.BalakinES.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Tyuiu.BalakinES.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.BalakinES.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.BalakinES.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TCheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
