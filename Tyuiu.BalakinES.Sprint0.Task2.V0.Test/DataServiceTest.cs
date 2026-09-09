a8dd995c42fe5776a9649a8a096c72bd260ae3d8
bes <bes237311@gmail.com>
2026-09-09 19:29:43 +0500

    Test2

---
+++ Tyuiu.BalakinES.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BalakinES.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BalakinES.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Егор";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Егор", res);
+        }
+    }
+}
